04961c486f3fdce21eb90cb60fc133196da4e038
OokamiMidori <fabio.miranda.couto@gmail.com>
2022-08-30 11:46:52 -0300

    aqui ta o trabalho

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+const par = Number(prompt("Escreva um numero par"))
+console.log("O resto da divisão de", par, "por 2, é:", par%2)
+
+//Todo numero par dividido por 2, resta zero da operação
+//Todo numero impar dividido por 2, sempre resta 1 da operação
---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+const num1 = Number(prompt("Insira um numero"))
+const num2 = Number(prompt("Insira outro numero"))
+console.log (num1>num2)
+console.log (num1===num2)
+
+const div1 = num1%num2===0
+const div2 = (num2%num1)===0
+
+console.log (div1)
+console.log (div2)
+
